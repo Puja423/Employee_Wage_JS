df4c44f8aa6d197ce45c170095d57f29d5213bef
Puja <pujayadav@gmail.com>
2020-11-18 11:35:38 +0530

    [puja]add. store data in array

--- Employee_Wage.js
+++ Employee_Wage.js
@@ -1,36 +1,43 @@
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const TOTAL_WORKING_DAYS = 20;
-const MAXIMUM_WORKING_HOURS = 100;
-function GetWorkingHours(empCheck)
-{ 
+
+const MAX_HRS_IN_MONTH=160;
+const NUM_OF_WORKING_DAYS=20;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const WAGE_PER_HOUR=20;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArr=new Array();
+ 
+function calcDailyWage(empHrs)
+{
+  return empHrs*WAGE_PER_HOUR;
+}
+ 
+function getWorkingHours(empCheck)
+{
     switch(empCheck)
     {
-        case IS_PART_TIME:
-        return PART_TIME_HOURS;
-        break;
-        case IS_FULL_TIME:
-        return FULL_TIME_HOURS;
-        break;
-        default:
-        return 0;
-        break;
-    }
-}
-let empHrs = 0;
-let totalWorkingDays = 0;
-for(let day = 0; day< TOTAL_WORKING_DAYS; day++)
-{
-   let empCheck = Math.floor(Math.random() * 10) % 3;
-   empHrs += GetWorkingHours(empCheck);
-   totalWorkingDays = day;
-   if(empHrs >= MAXIMUM_WORKING_HOURS)
-   {
-       break;
+       case IS_PART_TIME:
+            return 4;
+       case IS_FULL_TIME:
+            return 8;
+       default:
+            return 0;
    }
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total working days: " + totalWorkingDays + " Total Working hours: "+ empHrs + " Employee Wage: " + empWage);
+ 
+ 
+ 
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
+  totalWorkingDays++;
+  
+  let empCheck=Math.floor(Math.random()*10)%3;
+  let empHrs=getWorkingHours(empCheck);
+  totalEmpHrs+=empHrs;
+  empDailyWageArr.push(calcDailyWage(empHrs));
+}
+ 
+let empWage=calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+
